8963bce6c52571c3987e6b18efbbe44a51f594c3
ariete <deningliu@163.com>
2023-03-29 13:52:42 +0800

    新增抓取网卡所有数据包的函数

--- capture_packets.py
+++ capture_packets.py
@@ -8,5 +8,6 @@ def start_capture(interface_name):
 def packet_handler(packet):
     print(packet.summary())
 
+def sniff_all_packets(interface_name):
+    sniff(iface = interface_name, prn = packet_handler)
 
-sniff(iface="en0", prn=packet_handler)
