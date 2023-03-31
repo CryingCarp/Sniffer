a1b4d048794185ee539f31b5d91795cb069f2704
ariete <deningliu@163.com>
2023-03-31 16:00:20 +0800

    新增屏幕上打印输出二进制格式数据包函数

--- capture_packets.py
+++ capture_packets.py
@@ -1,13 +1,26 @@
 from scapy.all import *
 
 
-def start_capture(interface_name):
-    interface_object = conf.route.addrs[interface_name]
+def is_ipv4(packet):
+    if  type(packet) == scapy.layers.inet.IP:
+        print("true")
+        # print(packet.field.src)
+        # print(packet.field.dst)
+    else:
+        print("false")
 
+#在屏幕上打印输出数据包的二进制格式
+def print_hex_on_monitor(packet):
+    print(hexdump(packet))
 
-def packet_handler(packet):
+#在display窗口打印输出
+
+def sniff_all_packets(interface_name = 'en0'):
+    return sniff(iface = interface_name, count = 5)
+
+def print_on_monitor(packet):
     print(packet.summary())
 
-def sniff_all_packets(interface_name):
-    sniff(iface = interface_name, prn = packet_handler)
-
+packets_list = sniff_all_packets()
+for packet in packets_list:
+    print_hex_on_monitor(packet)
