a38dd23ab1a38ae1615ffc690cb1cf5cfc13bdd3
ariete <deningliu@163.com>
2023-04-12 22:24:09 +0800

    重写了开始嗅探函数，目前已经可以初步实现在界面中刷新显示抓取到的数据包

--- sniffer.py
+++ sniffer.py
@@ -1,10 +1,9 @@
 import sys
 from MainWindow import Ui_MainWindow
-from PyQt6.QtWidgets import QMainWindow, QApplication
-from logic import add_combo_items
-from ipconfig import get_interfaces_name
-import capture_packets
+from PyQt6.QtWidgets import QMainWindow, QApplication, QAbstractItemView, QTableWidget, QTableWidgetItem
 from MyThread import MyThread
+from ipconfig import get_interfaces_name
+from scapy.all import *
 
 class SnifferWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -13,25 +12,44 @@ class SnifferWindow(QMainWindow, Ui_MainWindow):
         self.display_interfaces_list()
         self.show()
 
-        self.sniff_button.clicked.connect(self.start_sniff)
+        #初始化抓包窗口
+        self.captured_view.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+        self.captured_view.setSelectionBehavior(QTableWidget.SelectionBehavior.SelectRows)
+        # self.captured_view.resizeColumnToContents()
 
+        # 初始化抓包线程
+        self.thread = MyThread()
+        self.thread.task_finished.connect(self.update_packets_list)
+        self.sniff_button.clicked.connect(self.start_sniff_thread)
 
     def set_captured_view_header(self):
         self.captured_view.setVerticalHeader()
+
     def display_interfaces_list(self):
         interifaces_list = get_interfaces_name()
         self.interfaces_combo.addItems(interifaces_list)
 
-    def start_sniff(self):
+    def start_sniff_thread(self):
+        # 设置按钮
         self.sniff_button.setEnabled(False)
         self.pause_button.setEnabled(True)
         self.resniff_button.setEnabled(True)
         self.stop_button.setEnabled(True)
-        # self.thread = MyThread(self.captured_view, self.captured_view, interface_name = self.interfaces_combo.currentData())
+        # 开始抓包前清除所有数据
+        self.captured_view.clearContents()
+        self.thread.start()
+
+    def update_packets_list(self, packet):
+        current_row_count = self.captured_view.rowCount()
+        self.captured_view.setRowCount(current_row_count + 1)
+        self.captured_view.setItem(current_row_count, 0, packet)
+        # self.captured_view.setItem(current_row_count, 1, packet)
+        # self.captured_view.setItem(current_row_count, 2, packet.)
+        # self.captured_view.setItem(current_row_count, 3, current_row_count + 1)
+        # self.captured_view.setItem(current_row_count, 4, current_row_count + 1)
+        # self.captured_view.setItem(current_row_count, 5, current_row_count + 1)
 
 
-    def display_captured_packets(self):
-        return
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
