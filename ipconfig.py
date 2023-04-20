5a54d0acc2aad2be8dbfa25714b8dbcca590f2ee
ariete <deningliu@163.com>
2023-04-20 19:36:42 +0800

    将获取网卡信息以及增加网卡选项功能集成到sniffer中

--- ipconfig.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-
-# 获取当前网络所有网卡信息
-# 返回一个列表
-def get_interfaces_name():
-    net_if_addrs = psutil.net_if_addrs()
-    interfaces_name = sorted(net_if_addrs.keys())
-    return interfaces_name
--- logic.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QComboBox
-
-
-# 在下拉三角框中添加网卡
-def add_combo_items(interfaces_list, ui):
-    if ui.interfaces_combo.addItems(interfaces_list):
-        return True
-    else:
-        return False
